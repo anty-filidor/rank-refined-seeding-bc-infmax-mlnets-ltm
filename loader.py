61200487fb6b62a23d6937b6c068175d184b164b
Michał Czuba <michal.czuba.1995@gmail.com>
2022-12-27 11:05:26 +0100

    added primitive exmerpimtn

--- loader.py
+++ loader.py
@@ -1,6 +1,16 @@
 import network_diffusion as nd
 
-
 net = nd.MultilayerNetwork.load_mlx(file_path="data/aucs.mpx")
 
-print(net)
+mltm = nd.models.MLTModel(
+    layers=list(net.layers.keys()),
+    protocol="AND",
+    seed_selector=nd.seeding.KShellSeedSelector(),
+    seeding_budget = (70, 30),
+    mi_value=0.4,
+)
+
+experiment = nd.MultiSpreading(model=mltm, network=net)
+logger = experiment.perform_propagation(n_epochs=10)
+logger.plot(True, ".")
+logger.report(path="./experiment_data")
