fd9c9ea9ba9db36b8c60c20aec85ba03859faefe
Michał Czuba <mc@vestigit.pl>
2023-03-10 10:26:13 +0100

    added config parsing

--- runner_main.py
+++ runner_main.py
@@ -1,127 +1,124 @@
 import itertools
+import yaml
 
 import network_diffusion as nd
-import numpy as np
 import pandas as pd
 
-from misc.loader import *
+from misc.net_loader import load_network
 from misc.utils import *
 from tqdm import tqdm
 
 
-# set_seed(43)  # in order to make reproducible results uncomment it!
+def parameter_space(protocols, seed_selector, seed_budgets, mi_values, networks):
+    ss = get_seed_selector(seed_selector["name"])(**seed_selector["parameters"])
+    seed_budgets_full = [(100 - i, i) for i in seed_budgets]
+    net_ranks = [
+        (n, _ := load_network(n), ss(_, actorwise=True)) for n in networks
+    ]  # network name, network, ranking
+    return itertools.product(protocols, seed_budgets_full, mi_values, net_ranks)
 
-SEED_SELECTOR = nd.seeding.DegreeCentralitySelector()
 
-PROTOCOLS = ("AND", )
-SEED_BUDGETS = [
-    # (100 - i, i) for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30]  # for OR
-    (100 - i, i) for i in [31, 32, 33, 34, 35, 36, 37, 38, 39, 40]  # for AND
-]
-MI_VALS = np.linspace(0.1, 0.9, num=9)
-NETS_RANKS = {
-    "aucs": [_ := get_aucs_network(), SEED_SELECTOR(_, actorwise=True)],
-    "ckm_physicians": [
-        _ := get_ckm_physicians_network(), SEED_SELECTOR(_, actorwise=True)
-    ],
-    "eu_transportation": [
-        _ := get_eu_transportation_network(), SEED_SELECTOR(_, actorwise=True)
-    ],
-    "lazega": [_ := get_lazega_network(), SEED_SELECTOR(_, actorwise=True)],
-    "er2": [_ := get_er2_network(), SEED_SELECTOR(_, actorwise=True)],
-    "er3": [_ := get_er3_network(), SEED_SELECTOR(_, actorwise=True)],
-    "er5": [_ := get_er5_network(), SEED_SELECTOR(_, actorwise=True)],
-    "sf2": [_ := get_sf2_network(), SEED_SELECTOR(_, actorwise=True)],
-    "sf3": [_ := get_sf3_network(), SEED_SELECTOR(_, actorwise=True)],
-    "sf5": [_ := get_sf5_network(), SEED_SELECTOR(_, actorwise=True)],
-}
+def run_experiments(config):
 
-MAX_EPOCHS_NUM = 1000
-PATIENCE = 1
-REPEATS_OF_EACH_CASE = determine_repetitions_for_selector(SEED_SELECTOR)
-FULL_LOGS_FREQ = 20 * REPEATS_OF_EACH_CASE
-OUT_DIR = prepare_out_path_for_selector(SEED_SELECTOR)
-
-global_stats_handler = pd.DataFrame(data={})
-experiments = itertools.product(PROTOCOLS, SEED_BUDGETS, MI_VALS, NETS_RANKS)
-p_bar = tqdm(list(experiments), desc="main loop", leave=False, colour="green")
-
-print(f"Experiments started at {get_current_time()}")
-
-for idx, investigated_case in enumerate(p_bar):
-
-    # obtain parameters of the propagation scenario
-    protocol = investigated_case[0]
-    seeding_budget = investigated_case[1]
-    mi_value = investigated_case[2]
-    network, ranking = NETS_RANKS[investigated_case[3]]
-
-    # initialise model - in order to speed up computations we use mocky selector
-    # and feed it with apriori computed ranking
-    mltm = nd.models.MLTModel(
-        protocol=protocol,
-        seed_selector=nd.seeding.MockyActorSelector(ranking),
-        seeding_budget = seeding_budget,
-        mi_value=mi_value,
+    p_space = parameter_space(
+        protocols=config["model"]["parameters"]["protocols"],
+        seed_budgets=config["model"]["parameters"]["seed_budgets"],
+        mi_values=config["model"]["parameters"]["mi_values"],
+        seed_selector=config["model"]["parameters"]["ss_method"],
+        networks=config["networks"],
     )
 
-    # perform experiment on given model and network "REPEATS" times
-    for repetition in range(1, 1 + REPEATS_OF_EACH_CASE):
+    max_epochs_num = config["run"]["max_epochs_num"]
+    patience = config["run"]["patience"]
+    repeats_of_each_case = config["run"]["repetitions"]
+    logging_freq = config["logging"]["full_output_frequency"]
+    out_dir = Path(config["logging"]["out_dir"]) / config["logging"]["name"]
+    out_dir.mkdir(exist_ok=True, parents=True)
 
-        # update progress_bar
-        case_name = (
-            f"proto_{protocol}--a_seeds_{seeding_budget[1]}"
-            f"--mi_{round(mi_value, 3)}--net_{investigated_case[3]}"
-            f"--run_{repetition}_{REPEATS_OF_EACH_CASE}"
-        )
-        p_bar.set_description_str(str(case_name))
+    global_stats_handler = pd.DataFrame(data={})
+    p_bar = tqdm(list(p_space), desc="main loop", leave=False, colour="green")
 
-        try:
-            # run experiment on a deep copy of the network!
-            experiment = nd.MultiSpreading(model=mltm, network=network.copy())
-            logs = experiment.perform_propagation(
-                n_epochs=MAX_EPOCHS_NUM, patience=PATIENCE
-            )
+    print(f"Experiments started at {get_current_time()}")
 
-            # obtain global data and if case is even local one as well
-            diffusion_len, active_actors, seed_actors = extract_basic_stats(
-                detailed_logs=logs._local_stats, patience=PATIENCE
-            )
-            active_actors_prct = active_actors / network.get_actors_num() * 100
-            seed_actors_prct = seed_actors / network.get_actors_num() * 100
-            gain = compute_gain(seed_actors_prct, active_actors_prct)
-            if idx % FULL_LOGS_FREQ == 0:
-                case_dir = OUT_DIR.joinpath(f"{idx}-{case_name}")
-                case_dir.mkdir(exist_ok=True)
-                logs.report(path=str(case_dir))
+    for idx, investigated_case in enumerate(p_bar):
 
-        except BaseException as e:
-            # print corrupted case 
-            diffusion_len = None
-            active_actors_prct = None
-            seed_actors_prct = None
-            gain = None
-            print(f"Ooops something went wrong for case: {case_name}: {e}")
+        # obtain parameters of the propagation scenario
+        protocol = investigated_case[0]
+        seeding_budget = investigated_case[1]
+        mi_value = investigated_case[2]
+        net_name, net, ranking = investigated_case[3]
 
-        # update global logs
-        case = {
-            "network": investigated_case[3],
-            "protocol": protocol,
-            "seeding_budget": seeding_budget[1],
-            "mi_value": mi_value,
-            "repetition_run": repetition,
-            "diffusion_len": diffusion_len,
-            "active_actors_prct": active_actors_prct,
-            "seed_actors_prct": seed_actors_prct,
-            "gain": gain,
-        }
-        global_stats_handler = pd.concat(
-            [global_stats_handler, pd.DataFrame.from_records([case])],
-            ignore_index=True,
-            axis=0,
+        # initialise model - in order to speed up computations we use mocky 
+        # selector and feed it with apriori computed ranking
+        mltm = nd.models.MLTModel(
+            protocol=protocol,
+            seed_selector=nd.seeding.MockyActorSelector(ranking),
+            seeding_budget = seeding_budget,
+            mi_value=mi_value,
         )
 
+        # perform experiment on given model and network "REPEATS" times
+        for repetition in range(1, 1 + repeats_of_each_case):
 
-global_stats_handler.to_csv(OUT_DIR.joinpath("results.csv"))
+            # update progress_bar
+            case_name = (
+                f"proto_{protocol}--a_seeds_{seeding_budget[1]}"
+                f"--mi_{round(mi_value, 3)}--net_{net_name}"
+                f"--run_{repetition}_{repeats_of_each_case}"
+            )
+            p_bar.set_description_str(str(case_name))
 
-print(f"Experiments finished at {get_current_time()}")
+            try:
+                # run experiment on a deep copy of the network!
+                experiment = nd.MultiSpreading(model=mltm, network=net.copy())
+                logs = experiment.perform_propagation(
+                    n_epochs=max_epochs_num, patience=patience
+                )
+
+                # obtain global data and if case is even local one as well
+                diffusion_len, active_actors, seed_actors = extract_basic_stats(
+                    detailed_logs=logs._local_stats, patience=patience
+                )
+                active_actors_prct = active_actors / net.get_actors_num() * 100
+                seed_actors_prct = seed_actors / net.get_actors_num() * 100
+                gain = compute_gain(seed_actors_prct, active_actors_prct)
+                if idx % logging_freq == 0:
+                    case_dir = out_dir.joinpath(f"{idx}-{case_name}")
+                    case_dir.mkdir(exist_ok=True)
+                    logs.report(path=str(case_dir))
+
+            except KeyboardInterrupt as e:
+                raise e
+
+            except BaseException as e:
+                # print corrupted case 
+                diffusion_len = None
+                active_actors_prct = None
+                seed_actors_prct = None
+                gain = None
+                print(f"Ooops something went wrong for case: {case_name}: {e}")
+
+            # update global logs
+            case = {
+                "network": net_name,
+                "protocol": protocol,
+                "seeding_budget": seeding_budget[1],
+                "mi_value": mi_value,
+                "repetition_run": repetition,
+                "diffusion_len": diffusion_len,
+                "active_actors_prct": active_actors_prct,
+                "seed_actors_prct": seed_actors_prct,
+                "gain": gain,
+            }
+            global_stats_handler = pd.concat(
+                [global_stats_handler, pd.DataFrame.from_records([case])],
+                ignore_index=True,
+                axis=0,
+            )
+
+    # save global logs and config
+    global_stats_handler.to_csv(out_dir.joinpath("results.csv"))
+    with open(out_dir / "config.yaml", "w") as f:
+        yaml.dump(config, f)
+
+    print(f"Experiments finished at {get_current_time()}")
