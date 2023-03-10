a6386e0347064a0d96280bed52a1433f172fcd10
Michał Czuba <michal.czuba.1995@gmail.com>
2023-03-10 13:47:55 +0100

    moved network data

--- misc/net_loader.py
+++ misc/net_loader.py
@@ -19,44 +19,44 @@ def get_aucs_network():
 
 def get_ckm_physicians_network():
     return _network_from_pandas(
-        "data/CKM-Physicians-Innovation_4NoNature.edges"
+        "data/networks/CKM-Physicians-Innovation_4NoNature.edges"
     )
 
 
 def get_eu_transportation_network():
     return _network_from_pandas(
-        "data/EUAirTransportation_multiplex_4NoNature.edges"
+        "data/networks/EUAirTransportation_multiplex_4NoNature.edges"
     )
 
 
 def get_lazega_network():
     return _network_from_pandas(
-        "data/Lazega-Law-Firm_4NoNatureNoLoops.edges"
+        "data/networks/Lazega-Law-Firm_4NoNatureNoLoops.edges"
     )
 
 
 def get_er2_network():
-    return nd.MultilayerNetwork.from_mpx(file_path="data/er_2.mpx")
+    return nd.MultilayerNetwork.from_mpx(file_path="data/networks/er_2.mpx")
 
 
 def get_er3_network():
-    return nd.MultilayerNetwork.from_mpx(file_path="data/er_3.mpx")
+    return nd.MultilayerNetwork.from_mpx(file_path="data/networks/er_3.mpx")
 
 
 def get_er5_network():
-    return nd.MultilayerNetwork.from_mpx(file_path="data/er_5.mpx")
+    return nd.MultilayerNetwork.from_mpx(file_path="data/networks/er_5.mpx")
 
 
 def get_sf2_network():
-    return nd.MultilayerNetwork.from_mpx(file_path="data/sf_2.mpx")
+    return nd.MultilayerNetwork.from_mpx(file_path="data/networks/sf_2.mpx")
 
 
 def get_sf3_network():
-    return nd.MultilayerNetwork.from_mpx(file_path="data/sf_3.mpx")
+    return nd.MultilayerNetwork.from_mpx(file_path="data/networks/sf_3.mpx")
 
 
 def get_sf5_network():
-    return nd.MultilayerNetwork.from_mpx(file_path="data/sf_5.mpx")
+    return nd.MultilayerNetwork.from_mpx(file_path="data/networks/sf_5.mpx")
 
 
 def load_network(net_name: str) -> nd.MultilayerNetwork:
