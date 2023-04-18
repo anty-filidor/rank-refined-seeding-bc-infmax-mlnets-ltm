a1b721c6a00a1f408632a3094b516a7559dc1980
unknown <damiandbrowski@yahoo.pl>
2023-04-18 12:56:21 +0200

    ICM1

--- Test.py
+++ Test.py
@@ -2,8 +2,12 @@ from typing import Dict, List, Tuple
 
 import networkx as nx
 import numpy as np
-
+from network_diffusion.models.ICM_model2 import ICModel as IC2
+from network_diffusion.models.ICM_model import ICModel
+from network_diffusion.mln.mlnetwork import MultilayerNetwork as mln
 from network_diffusion.models.mlt_model import MLTModel
+from network_diffusion.seeding.neighbourhoodsize_selector import NeighbourhoodSizeSelector
+from network_diffusion.multi_spreading import  MultiSpreading
 from network_diffusion.seeding.kshell_selector import  KShellSeedSelector, KShellMLNSeedSelector
 from network_diffusion.mln.actor import MLNetworkActor
 from network_diffusion.mln.mlnetwork import MultilayerNetwork as MLNetwork
@@ -12,20 +16,20 @@ from network_diffusion.models.base_model import NetworkUpdateBuffer as NUBuff
 from network_diffusion.models.utils.compartmental import CompartmentalGraph
 from network_diffusion.seeding.base_selector import BaseSeedSelector
 from network_diffusion.utils import BOLD_UNDERLINE, THIN_UNDERLINE, NumericType
-tup = (70,30)
+tup = (50,50)
 
-model1 = []
+#model1 = []
 #site = MLNetwork()
-ml = MLNetwork.from_mpx("D:/Zajęcia/Studia magisterskie/Do pracy magisterskiej/python/ltm-seeding-mln/data/networks/er_2.mpx")
-#print(MLNetwork._get_description_str(ml))
-G = nx.Graph()
-#nx.draw(G)
-#seed1 = BaseSeedSelector()
-seed = KShellMLNSeedSelector()
+ml = MLNetwork.from_mpx("D:/Zajęcia/Studia magisterskie/Do pracy magisterskiej/python/Independent-Cascade-Model/data/networks/er_2.mpx")
+seed = NeighbourhoodSizeSelector()
 model = MLTModel(tup, seed, "OR", 0.5)
-m1=model.set_initial_states(ml)
 
-model1 = model.network_evaluation_step(ml)
-m2 = model.get_allowed_states(ml)
-#print(list(model1))
-print(m2)
+icm = ICModel(tup,seed,"OR",0.15)
+icm2 = IC2(tup,seed,"AND",0.15)
+icm2.set_initial_states(ml)
+prop = MultiSpreading(icm,ml)
+print(prop.perform_propagation(2))
+#print(len(icm2.symulacja(ml))) # symulacja zwraca losowe wyniki czyli pr
+#print(model.__str__())
+
+print(len(icm2.network_evaluation_step(ml)))
