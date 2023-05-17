2ad8d9f3be789d549dc6b3f1a6e03bc0fe978241
Damian Dąbrowski <damiandbr75@gmail.com>
2023-05-17 12:19:41 +0200

    Test random-seed

--- Test.py
+++ Test.py
@@ -1,9 +1,14 @@
 from typing import Dict, List, Tuple
-
+import os
+import sys
 import networkx as nx
+import random
 import numpy as np
-from network_diffusion.models.ICM_model2 import ICModel as IC2
-from network_diffusion.models.ICM_model import ICModel
+from network_diffusion.models.mic_model import MICModel
+from network_diffusion.experiment_logger import ExperimentLogger
+from network_diffusion.seeding.degreecentrality_selector import DegreeCentralitySelector
+from network_diffusion.models.mic_model import MICModel
+from network_diffusion.seeding.random_selector import RandomSeedSelector
 from network_diffusion.mln.mlnetwork import MultilayerNetwork as mln
 from network_diffusion.models.mlt_model import MLTModel
 from network_diffusion.seeding.neighbourhoodsize_selector import NeighbourhoodSizeSelector
@@ -16,20 +21,31 @@ from network_diffusion.models.base_model import NetworkUpdateBuffer as NUBuff
 from network_diffusion.models.utils.compartmental import CompartmentalGraph
 from network_diffusion.seeding.base_selector import BaseSeedSelector
 from network_diffusion.utils import BOLD_UNDERLINE, THIN_UNDERLINE, NumericType
-tup = (50,50)
+tup = (10, 90)
 
-#model1 = []
-#site = MLNetwork()
-ml = MLNetwork.from_mpx("D:/Zajęcia/Studia magisterskie/Do pracy magisterskiej/python/Independent-Cascade-Model/data/networks/er_2.mpx")
-seed = NeighbourhoodSizeSelector()
-model = MLTModel(tup, seed, "OR", 0.5)
+#seed = RandomSeedSelector()
 
-icm = ICModel(tup,seed,"OR",0.15)
-icm2 = IC2(tup,seed,"AND",0.15)
-icm2.set_initial_states(ml)
-prop = MultiSpreading(icm,ml)
-print(prop.perform_propagation(2))
-#print(len(icm2.symulacja(ml))) # symulacja zwraca losowe wyniki czyli pr
-#print(model.__str__())
+ml = MLNetwork.from_mpx(
+    "D:/Zajęcia/Studia magisterskie/Do pracy magisterskiej/python/Independent-Cascade-Model/data/networks/er_3.mpx")
 
-print(len(icm2.network_evaluation_step(ml)))
+# random seed
+
+random.seed('a28')
+seed = RandomSeedSelector()
+
+
+print(random.random())
+model = MICModel(tup,seed,'OR', 0.1)
+
+spred = MultiSpreading(model,ml)
+print(spred.perform_propagation(4))
+
+
+#graph: nx.Graph() = ml.layers['l1']
+#print(list(graph.neighbors('a630')))
+
+
+
+
+
+
